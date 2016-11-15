984d837ed5337b19eed4a217dfd0098d172605da
lsy-android <lsy1234567@163.com>
2016-11-15 20:36:26 +0800

    修复登陆时的查询的问题

--- routes/login.js
+++ routes/login.js
@@ -8,25 +8,24 @@ module.exports = function(app) {
     var User = global.dbHelper.getModel('user')
     var uname = req.body.username
 
-    User.find({
+    User.findOne({
       name: uname
-    },{
-      name: 1,
-      passord: 1,
-      _id: 0
     }, function(error, doc) {
       if(error) {
         console.log(uname + '用户不存在:'+error)
         req.session.error = '用户不存在'
         res.sendStatus(404)
-      } else if(doc.password !== req.body.password) {
-        console.log('密码错误！' + doc)
-        req.session.error = '密码错误！'
-        res.sendStatus(404)
       } else {
-        console.log('登陆成功！' + doc)
-        res.session.user = doc
-        res.sendStatus(200)
+        console.log('获取的密码为：'+doc.password)
+        if(doc.password !== req.body.password) {
+          console.log('密码错误！' + doc)
+          req.session.error = '密码错误！'
+          res.sendStatus(404)
+        } else {
+          console.log('登陆成功！' + doc)
+          req.session.user = doc
+          res.sendStatus(200)
+        }
       }
     })
   })
